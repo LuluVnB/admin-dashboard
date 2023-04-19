73935202ae28d569d6d19fb6f3c1a08d295ea3c8
Gian <paraguya.gian@gmail.com>
2023-04-19 10:26:37 -0700

    project content icons now added and have margins

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ for(let i = 0; i < 6; i++){
 
     const projectBoxContent = document.createElement("div");
     projectBox.appendChild(projectBoxContent);
-    
+    projectBoxContent.className = "project-box-content";
+
+    const projectContentText = document.createElement("div");
+    projectBoxContent.appendChild(projectContentText);
+    projectContentText.className = "project-content-text"
+
+    const projectContentIcons = document.createElement("div");
+    projectBoxContent.appendChild(projectContentIcons);
+    projectContentIcons.className = "project-content-icons"
+
+    const starIcon = document.createElement("img");
+    projectContentIcons.appendChild(starIcon);
+    starIcon.src = "./icons/star-plus-icon.svg";
+    starIcon.style.width = "1.7rem";
+    starIcon.style.marginRight = "15px"
+
+    const eyeIcon = document.createElement("img");
+    projectContentIcons.appendChild(eyeIcon);
+    eyeIcon.src = "./icons/eye-plus-icon.svg";
+    eyeIcon.style.width = "1.7rem";
+    eyeIcon.style.marginRight = "15px";
+
+    const linkIcon = document.createElement("img");
+    projectContentIcons.appendChild(linkIcon);
+    linkIcon.src = "./icons/link-icon.svg";
+    linkIcon.style.width = "1.7rem";
+    linkIcon.style.marginRight = "15px";
 }
 
